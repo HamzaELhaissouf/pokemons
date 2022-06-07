47445964e76db58e21d5e1c6b74dd8305f7c9ea6
Hamza El haissouf <el1haissoufhamza@gmail.com>
2022-06-07 23:38:06 +0100

    fix modal

--- src/components/items/ItemModal.tsx
+++ src/components/items/ItemModal.tsx
@@ -35,17 +35,8 @@ const ItemModal = () => {
     return (
         <Transition.Root show={showModal && !!item} as={Fragment}>
             <Dialog as="div" className="relative z-10" onClose={closeModal}>
-                <Transition.Child
-                    as={Fragment}
-                    enter="ease-out duration-300"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
-                    leave="ease-in duration-200"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                >
-                    <div className="hidden fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity md:block" />
-                </Transition.Child>
+
+                <div className="hidden fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity md:block" />
 
                 <div className="fixed z-10 inset-0 overflow-y-auto">
                     <div className="flex Items-stretch  md:Items-center justify-center min-h-full text-center md:px-2 lg:px-4">
